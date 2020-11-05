e43a4fcb62d312c903cdb946c399036adb72a24f
Istvan Halasz <istvan.halasz@pentalog.com>
2020-11-05 19:09:49 +0200

    update array immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -14,9 +14,12 @@ class Counter extends Component {
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter} />
                 <hr />
-                <button>Store Result</button>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
                 <ul>
-                    <li></li>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                    ))}
+
                 </ul>
             </div>
         );
@@ -25,7 +28,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 }
 
@@ -35,7 +39,8 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
         onAddCounter: () => dispatch({ type: 'ADD', value: 5 }),
         onSubtractCounter: () => dispatch({ type: 'SUBTRACT', value: 5 }),
-        onStoreResult: () => dispatch({ type: 'STORE_RESULT', value: 5 })
+        onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+        onDeleteResult: (id) => dispatch({ type: 'DELETE_RESULT', resultElementId: id })
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,20 +6,36 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
+            const newState = Object.assign({}, state);//immutable copy of the state object
+            newState.counter = state.counter + 1;
+            return newState;
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.value
             }
         case 'SUBTRACT':
             return {
+                ...state,
                 counter: state.counter - action.value
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({ id: new Date(), value: state.counter })
+            }
+        case 'DELETE_RESULT':
+            //const newArray = [...state.results];
+            //newArray.results.splice(2, 1);
+            const updatedArray = state.results.filter((result, index) => result.id !== action.resultElementId);//create a new array
+            return {
+                ...state,
+                results: updatedArray
             }
 
     }
