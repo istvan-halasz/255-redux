95ee88f1eb42b0509631ea439a3a904a3f06466e
Istvan Halasz <istvan.halasz@pentalog.com>
2020-11-05 18:29:02 +0200

    redux starting

---
+++ src/containers/Counter/Counter.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter} />
+                <hr />
+                <button>Store Result</button>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
+        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
+        onAddCounter: () => dispatch({ type: 'ADD', value: 5 }),
+        onSubtractCounter: () => dispatch({ type: 'SUBTRACT', value: 5 }),
+        onStoreResult: () => dispatch({ type: 'STORE_RESULT', value: 5 })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
