8b53426734be456d26be5f57a7f23718bcde1259
Istvan Halasz <istvan.halasz@pentalog.com>
2020-11-05 19:28:18 +0200

    using constants

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions'
+
 const initialState = {
     counter: 0,
     results: []
@@ -5,31 +7,31 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             const newState = Object.assign({}, state);//immutable copy of the state object
             newState.counter = state.counter + 1;
             return newState;
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return {
                 ...state,
                 counter: state.counter - 1
             }
-        case 'ADD':
+        case actionTypes.ADD:
             return {
                 ...state,
                 counter: state.counter + action.value
             }
-        case 'SUBTRACT':
+        case actionTypes.SUBTRACT:
             return {
                 ...state,
                 counter: state.counter - action.value
             }
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return {
                 ...state,
                 results: state.results.concat({ id: new Date(), value: state.counter })
             }
-        case 'DELETE_RESULT':
+        case actionTypes.DELETE_RESULT:
             //const newArray = [...state.results];
             //newArray.results.splice(2, 1);
             const updatedArray = state.results.filter((result, index) => result.id !== action.resultElementId);//create a new array
